c8389fd5477eebf6a75de5ec41c534c878160ecc
Nikolay Dimolarov <dimolarov@hotmail.com>
2018-05-28 17:41:09 +0300

    minor refactoring of auth

--- controllers/userController.js
+++ controllers/userController.js
@@ -18,41 +18,44 @@ exports.register = (req, res) => {
       return res.status(200).send({ user: user });
     })
     .catch(err => {
+      console.log(err);
       return res.status(500).send("There was a problem registering the user.");
     });
 };
 
 exports.auth = (req, res) => {
-  User.findOne({ 'username': req.body.username }, '_id username fullName email password createdAt', (err, user) => {
-    if (err) { return res.status(500).send(err); }
-    if (!user) { return res.status(500).send("User cannot be found."); }
+  User.findOne({
+    'username': req.body.username },
+    '_id username fullName email password createdAt',
+    (err, user) => {
+      if (err) { return res.status(500).send(err); }
+      if (!user) { return res.status(500).send("User cannot be found."); }
 
-    // check password if user is found
-    bcrypt.compare(req.body.password, user.password)
-      .then(bcryptRes => {
-        if (!bcryptRes) { return res.status(500).send("Wrong password.")}
-        if (bcryptRes) {
-          const token = jwt.sign({ id: user._id }, 'secret', {
-            expiresIn: 86400 // expires in 24 hours
-          });
+      // check password if user is found
+      bcrypt.compare(req.body.password, user.password)
+        .then(bcryptRes => {
+          if (!bcryptRes) { return res.status(500).send("Wrong password.")}
+          if (bcryptRes) {
+            const token = jwt.sign({ id: user._id }, 'secret', {
+              expiresIn: 86400 // expires in 24 hours
+            });
 
-          const userResponse = {
-            _id: user._id,
-            username: user.username,
-            fullName: user.fullName,
-            email: user.email,
-            createdAt: user.createdAt
-          };
+            const userResponse = {
+              _id: user._id,
+              username: user.username,
+              fullName: user.fullName,
+              email: user.email,
+              createdAt: user.createdAt
+            };
 
-          return res.status(200).send({ user: userResponse, token: token });
-        }
-      })
-      .catch(err => {
-        console.log(err);
-        return res.status(500).send("Something went wrong with bcrypt.");
-      });
-  });
-}
+            return res.status(200).send({ user: userResponse, token: token });
+          }
+        })
+        .catch(bcryptErr => {
+          return res.status(500).send("Something went wrong with bcrypt.");
+        });
+    });
+};
 
 exports.test = (req, res) => {
   return res.status(200).send({ test: true });
